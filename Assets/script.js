3c65d3c531213d5bce5f9a95d993ecd4b98cad23
Georgia Milteer <gmilteer@gmail.com>
2020-07-24 22:40:58 -0400

    second push

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,31 @@
 $(document).ready(function () {
  
-
+// Empties array of saved cities from user imput
   var savedCities = [];
-
+// click function on search button displays user input onto the city div and data from AJAX request onto the corresponding divs
   $("#search-button").on("click", function () {
-    var userInput = $("#city-name").val();
     var city = $("#current-city");
-    var saveCity = $("userInput");
-    var newSearch = $("<button type=button class=btn-lg>").text(userInput);
-    $("#new-div").append(newSearch);
-    city.text(userInput + "|");
+    
+   
+    var userInput = $("#city-name").val();
+
+    if(savedCities.includes(userInput)) {
+      return;
+    } else {
+      savedCities.push(userInput);
+      var newSearch = $("<button type='button' id='newSearch' class='search-btn btn-lrg'>").text(userInput)
+      $("#new-div").append(newSearch);
+    }
+    city.text(userInput + " | ")
     cityName = userInput;
 
     getWeather(userInput);
     displayDate();
+    
+   
         });
     });
+    // click even for city buttons
     $(document).on("click", ".search-btn", function () {
         var city = $("#currentCity");
         getWeather($(this).text());
@@ -23,11 +33,12 @@ $(document).ready(function () {
         city.text(citySearch + " | ");
 
     });
-
+    // Function to display date
     function displayDate() {
         var date = moment().format(("M" + "-" + "D" + "-" + "YYYY") + " |");
         $("#currentDate").text(date);
     };
+    // Function that retrieves weather data
     function getWeather(userInput) {
 
     var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=806cb968e80e73594a933189dd65df65";
@@ -36,11 +47,11 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       var temp = Math.floor((response.main.temp - 273.15) * 1.8 + 32);
-      $("#temperature").text(temp);
-      $("#humidity").text(response.main.humidity);
-      $("#wind-speed").text(response.wind.speed);
+      $("#temperature").text(temp + "°F");
+      $("#humidity").text(response.main.humidity + "%");
+      $("#wind-speed").text(response.wind.speed + "mph");
       console.log(response);
-
+     
       var lat = response.coord.lat;
       var lng = response.coord.lon;
       var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&appid=806cb968e80e73594a933189dd65df65";
@@ -50,9 +61,33 @@ $(document).ready(function () {
         method: "GET",
       }).then(function (response) {
         console.log(response);
+        var uvi =response.current.uvi;
         $("#uv-index").text(response.current.uvi);
+        $("#uv-index").text(uvi);
+        if (uvi <= 2) {
+          $("uv-index").css({
+            "background-color": "#88BDBC",
+            "padding" : "10px 10px 10px 10px",
+            "border" : "2px solid #343a3f",
+          });
+        } else if (uvi <= 5 && uvi > 2) {
+          $("#uv-index").css({
+            "background-color": "F3D250",
+            "padding" : "10px 10px 10px 10px",
+            "border" : "2px solid #343a3F",
+          });
+        } else {
+          $("#uv-index").css ({
+            "background-color": "#F78888",
+            "padding": "10px 10px 10px 10px",
+            "border": "2px solid #343A3F",
+          });
+        };
+      
       });
     });
+        
+
 // 5-Day Forecast API
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + userInput + "&appid=e189f3ac24747877cecf335702c81a6e"
 $.ajax({
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Weather-Dashboard-HW
+Weather-Dashboard
+Weather-Dashboard is an interactive application that utilizes server-side APIs to display real-time weather data across the many cities around the world.
+
+Description
+Once the user enters the applciation, users are greeted with a polished weather dashboard featuring a city search field and weather descriptions with empty data fields.
+
+Once the user inputs a city name into the search box, weather data is projected next to the corresponding descriptors. The top portion of the weather dashboard displays current weather data, whereas the bottom half of the dashboard features a 5-day forecast. Below is a breakdown of the specific data featured on the site.
+
+Current Weather Tracks: 1. Temperature 2. Humidity 3. Wind Speed 4. UV Index
+
+5-Day Forecast Tracks: 1. Date 2. Temperature 3. Humidity
+
+Lastly, users have the ability to enter as many cities as they so choose and click on formerly searched cities to re-display city-specific weather data.
+
+Visuals
+![](./Assets/Images/Weather-Dashboard.png)
+
